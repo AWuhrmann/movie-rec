d9efa6d4f0cc1c08a1c5ade00fcc42bc79511ea1
arthur <arthur.wuhrmann@epfl.ch>
2024-12-14 17:57:09 +0100

    I hope I fixed the issue with resizing main windows

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -41,7 +41,7 @@
   <!-- Main content -->
   <div class="flex justify-center">
     <!-- Using larger max-width and adjusted breakpoints -->
-    <div class="pl-64 [@media(min-width:1400px)]:pl-0 [@media(min-width:1400px)]:w-[1000px] mx-auto">
+    <div class="pl-64 w-[1400px] min-w-0 [@media(min-width:1500px)]:pl-0 [@media(min-width:1500px)]:w-[1000px] mx-auto">
       <main class="min-h-screen p-8">
         {@render children()}
       </main>
